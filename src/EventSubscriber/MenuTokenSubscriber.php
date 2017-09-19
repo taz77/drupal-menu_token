9cfccf0a8c67fd70a82ebc55d3d30d059b8efc1d
peterdraucbaher <peter.draucbaher@agiledrop.com>
2017-09-19 22:30:08 +0200

    Remove duplicate caching  and possible problematic seriliazation.

--- src/EventSubscriber/MenuTokenSubscriber.php
+++ src/EventSubscriber/MenuTokenSubscriber.php
@@ -24,20 +24,10 @@ class MenuTokenSubscriber implements EventSubscriberInterface {
   /**
    * MenuTokenSubscriber constructor.
    *
-   * @param \Drupal\Core\Session\AccountProxy $currentUser
-   *   Current user.
-   * @param \Drupal\Core\Cache\CacheBackendInterface $cache
-   *   Cache.
-   * @param \Drupal\Core\Plugin\Context\ContextRepositoryInterface $contextRepository
-   *   Context repository service.
    * @param \Drupal\menu_token\Service\MenuTokenContextManager $menuTokenContextManager
    *   Menu token context manager.
    */
-  public function __construct(AccountProxy $currentUser, CacheBackendInterface $cache, ContextRepositoryInterface $contextRepository, MenuTokenContextManager $menuTokenContextManager) {
-
-    $this->currentUser = $currentUser;
-    $this->cache = $cache;
-    $this->contextRepository = $contextRepository;
+  public function __construct(MenuTokenContextManager $menuTokenContextManager) {
     $this->menuTokenContextManager = $menuTokenContextManager;
   }
 
@@ -50,26 +40,10 @@ class MenuTokenSubscriber implements EventSubscriberInterface {
    *   For handling a request. Constant KernelEvents::CONTROLLER.
    */
   public function onController(FilterControllerEvent $event) {
-    $current_user_id = $this->currentUser->id();
 
-    /*
-     * Use cache tag named menu_token_cached_context to store the
-     * sh1 of serialized sh1 string of context returned by context service.
-     * If context on current request is the same as context on previous
-     * request then replacement is not executed.
-     *
-     * If it is not the menu_token.context_manager service takes over
-     * and replace the menu links from state object.
-     *
-     */
-    $cache = $this->cache->get('menu_token_cached_context' . $current_user_id);
-    $contexts_def = $this->contextRepository->getAvailableContexts();
-    $runtime_context = $this->contextRepository->getRuntimeContexts(array_keys($contexts_def));
+    $this->menuTokenContextManager->replaceContextualLinks();
 
-    if (empty($cache->data) || sha1(serialize($runtime_context)) != sha1(serialize($cache->data))) {
-      $this->cache->set('menu_token_cached_context' . $current_user_id, $runtime_context, -1, ['menu_token_cached_context_tag' . $current_user_id]);
-      $this->menuTokenContextManager->replaceContextualLinks();
-    }
+
   }
 
   /**
