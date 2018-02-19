baa41e6374de922b58b9916c9e31384dc5cc2867
nhaarish <nhaarish@3388728.no-reply.drupal.org>
2018-02-19 10:50:50 +0530

    Issue #2932996 by nhaarish: No active trail for menu link with token

--- src/Service/MenuLinkTreeMenuToken.php
+++ src/Service/MenuLinkTreeMenuToken.php
@@ -74,6 +74,7 @@ class MenuLinkTreeMenuToken extends MenuLinkTree {
    */
   protected function buildItems(array $tree, CacheableMetadata &$tree_access_cacheability, CacheableMetadata &$tree_link_cacheability) {
     $items = [];
+	$current_path = \Drupal::request()->getRequestUri();
 
     foreach ($tree as $data) {
       /** @var \Drupal\Core\Menu\MenuLinkInterface $link */
@@ -142,6 +143,10 @@ class MenuLinkTreeMenuToken extends MenuLinkTree {
       if (isset($data->options)) {
         $element['url']->setOptions(NestedArray::mergeDeep($element['url']->getOptions(), $data->options));
       }
+	  if ($element['url']->toString() == $current_path) {
+        $element['url']->setOption('attributes', array('class' => array('is-active')));
+      }
+
       $element['original_link'] = $link;
       $items[$link->getPluginId()] = $element;
     }
